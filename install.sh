27aeacd0deddf6e052ef59e538c8e97f83df1bd3
Lucas Pires <lucas.pires.mattos@gmail.com>
2018-10-13 14:07:23 -0300

    install: add fonts

--- install.sh
+++ install.sh
@@ -25,3 +25,24 @@ if [[ ! -f /usr/bin/docker-compose ]]; then
   echo "Installing Docker Compose..."
   sudo pacman -S --noconfirm docker-compose
 fi
+
+# Install Fonts
+echo "Installing fonts..."
+sudo pacman -S --noconfirm \
+  ttf-roboto \
+  ttf-liberation \
+  ttf-ubuntu-font-family \
+  ttf-fira-mono \
+  otf-fira-mono \
+  ttf-inconsolata \
+  adobe-source-code-pro-fonts \
+  ttf-linux-libertine \
+  adobe-source-sans-pro-fonts \
+  noto-fonts-emoji
+
+# Install Emoji Fonts
+yay -S --noconfirm \
+  ttf-symbola \
+  ttf-emojione \
+  ttf-emojione-color \
+  ttf-twemoji-color
